4038aa015a7dab76ac21cf876e3383cbd35a1b55
Mak Alamin <makalamin012@gmail.com>
2022-02-12 21:20:51 +0600

    MongoDB Connected and server started successfully

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(bodyParser.json( { limit: '50mb', extended: true } ));
+
+app.use(bodyParser.urlencoded( { limit: '50mb', extended: true } ));
+
+app.use(cors());
+
+const  CONNECTION_URL = process.env.CONNECTION_URL;
+
+const  PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen( PORT, () => console.log(`Server running on port ${PORT}`) ) )
+    .catch((error) => {
+        console.log(error.message);
+    });
+
+app.get('/', (req, res) => res.send('Hello From Server!'));
+app.get('/about', (req, res) => res.send('Hello From About Page.'));
