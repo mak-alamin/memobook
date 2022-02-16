7c007e1d7a33b26e9e27535fb7172b47ea2f2af0
Mak Alamin <makalamin012@gmail.com>
2022-02-16 20:22:33 +0600

    changed client to frontend

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello From Posts!');
+    res.send('Hello From Posts.');
 });
 
 export default router;
