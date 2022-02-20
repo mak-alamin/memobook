db82b1bb8f2f0468107634c8549723a046c0f0bc
Mak Alamin <makalamin012@gmail.com>
2022-02-21 00:10:05 +0600

    installed tailwind css

--- frontend/src/components/Profile/ProfileCard.js
+++ frontend/src/components/Profile/ProfileCard.js
@@ -4,14 +4,18 @@ import profilePic from "../../assets/images/user-1.jpg";
 function ProfileCard() {
   return (
     <>
-      <div className="profile-card bg-light p-3 mt-4">
-        <img src={profilePic} alt="user" className="profile-photo" />
+      <div className="profile-card relative bg-primary">
+        <img
+          src={profilePic}
+          alt="user"
+          className="profile-photo absolute top--25"
+        />
         <h5 className="mt-4">
-          <a href="timeline.html" className="">
+          <a href="timeline.html" className="text-light">
             Sarah Cruiz
           </a>
         </h5>
-        <a href="test.com" className="">
+        <a href="test.com" className="text-light">
           <i className="ion ion-android-person-add"></i> 1,299 followers
         </a>
       </div>
