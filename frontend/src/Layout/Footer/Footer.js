506348bd8d45097c76c86482d843f49a77d686ad
Mak Alamin <makalamin012@gmail.com>
2022-02-26 23:02:51 +0600

    added footer copyright

--- frontend/src/Layout/Footer/Footer.js
+++ frontend/src/Layout/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer bg-light py-3 mt-5">
       <div className="text-center">
-        <span>Footer content here.</span>
+        <small>&copy; 2022 | MemoBook, All rights reserved.</small>
       </div>
     </footer>
   );
